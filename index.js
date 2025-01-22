b02ecde04f1205ad534a3b4f8dbff05a8a2fe28b
Richard-4663 <richardrodriguesdossantosu@gmail.com>
2025-01-22 11:42:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ const long5 = document.getElementsByClassName("long5")[0];
 
 
 home.addEventListener("mouseover", () => {
-        long1.style.top = "60px"; // Atualiza o estilo diretamente
+        long1.style.top = "60px"; 
     long1.style.zIndex = "1"; 
 });
 
 long1.addEventListener("mouseover", () => {
-    long1.style.top = "60px"; // Atualiza o estilo diretamente
+    long1.style.top = "60px"; 
     long1.style.zIndex = "1";
 });
 
 
 long1.addEventListener("mouseout", () => {
-        long1.style.top = "-10px"; // Restaura o estilo original
+        long1.style.top = "-10px"; 
          long1.style.zIndex = "0";
 });
 
 home.addEventListener("mouseout", () => {
     setTimeout(() => {
-        long1.style.top = "-10px"; // Restaura o estilo original
+        long1.style.top = "-10px"; 
     long1.style.zIndex = "0";
     }, 400);
     
@@ -36,96 +36,131 @@ home.addEventListener("mouseout", () => {
 
 
 about.addEventListener("mouseover", () => {
-    long2.style.top = "60px"; // Atualiza o estilo diretamente
+    long2.style.top = "60px"; 
     long2.style.zIndex = "1";
 });
 
 long2.addEventListener("mouseover", () => {
-    long2.style.top = "60px"; // Atualiza o estilo diretamente
+    long2.style.top = "60px"; 
     long2.style.zIndex = "1";
 });
 
 
 long2.addEventListener("mouseout", () => {
-    long2.style.top = "-22px"; // Restaura o estilo original
+    long2.style.top = "-22px"; 
     long2.style.zIndex = "0";
 });
 
 about.addEventListener("mouseout", () => {
     setTimeout(() => {
-        long2.style.top = "-22px"; // Restaura o estilo original
+        long2.style.top = "-22px"; 
     long2.style.zIndex = "0";
     }, 400);
 });
 
 
 shop.addEventListener("mouseover", () => {
-    long3.style.top = "60px"; // Atualiza o estilo diretamente
+    long3.style.top = "60px"; 
     long3.style.zIndex = "1";
 });
 
 long3.addEventListener("mouseover", () => {
-    long3.style.top = "60px"; // Atualiza o estilo diretamente
+    long3.style.top = "60px"; 
     long3.style.zIndex = "1";
 });
 
 
 long3.addEventListener("mouseout", () => {
-    long3.style.top = "-10px"; // Restaura o estilo original
+    long3.style.top = "-10px"; 
     long3.style.zIndex = "0";
 });
 
 shop.addEventListener("mouseout", () => {
     setTimeout(() => {
-        long3.style.top = "-10px"; // Restaura o estilo original
+        long3.style.top = "-10px"; 
     long3.style.zIndex = "0";
     }, 400);
 });
 
 
 language.addEventListener("mouseover", () => {
-    long4.style.top = "60px"; // Atualiza o estilo diretamente
+    long4.style.top = "60px"; 
     long4.style.zIndex = "1";
 });
 
 long4.addEventListener("mouseover", () => {
-    long4.style.top = "60px"; // Atualiza o estilo diretamente
+    long4.style.top = "60px"; 
     long4.style.zIndex = "1";
 });
 
 
 long4.addEventListener("mouseout", () => {
-    long4.style.top = "-10px"; // Restaura o estilo original
+    long4.style.top = "-10px"; 
     long4.style.zIndex = "0";
 });
 
 language.addEventListener("mouseout", () => {
     setTimeout(() => {
-        long4.style.top = "-10px"; // Restaura o estilo original
+        long4.style.top = "-10px"; 
     long4.style.zIndex = "0";
     }, 400);
 });
 
 
 contact.addEventListener("mouseover", () => {
-    long5.style.top = "60px"; // Atualiza o estilo diretamente
+    long5.style.top = "60px"; 
     long5.style.zIndex = "1";
 });
 
 long5.addEventListener("mouseover", () => {
-    long5.style.top = "60px"; // Atualiza o estilo diretamente
+    long5.style.top = "60px"; 
     long5.style.zIndex = "1";
 });
 
 
 long5.addEventListener("mouseout", () => {
-    long5.style.top = "-10px"; // Restaura o estilo original
+    long5.style.top = "-10px"; 
     long5.style.zIndex = "0";
 });
 
 contact.addEventListener("mouseout", () => {
     setTimeout(() => {
-        long5.style.top = "-10px"; // Restaura o estilo original
+        long5.style.top = "-10px"; 
     long5.style.zIndex = "0";
     }, 400);
+});
+
+function abrirLogin(){
+    const login = document.getElementsByClassName("login-all")[0];
+
+    login.show();
+}
+
+function fecharLogin(){
+    const login = document.getElementsByClassName("login-all")[0];
+    
+        login.close();
+}
+
+const video = document.getElementById("input");
+const nav = document.getElementsByClassName("nav")[0];
+const longs = document.getElementsByClassName("longs")[0];
+const menu = document.getElementsByClassName("menu")[0];
+const nav_fix = document.getElementsByClassName("nav-fix")[0];
+
+video.addEventListener("loadeddata", () => {
+    video.play();
+})
+
+
+video.addEventListener("ended", () => {
+    video.style.opacity = "0";
+    video.style.pointerEvents = "none";
+    nav.style.opacity = 1;
+    nav_fix.style.opacity = 1;
+    
+    setTimeout(() => {
+        menu.style.opacity = 1;
+        longs.style.opacity = 1;
+    }, 1000);
 });
